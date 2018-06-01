67b6214e2db99ae3c21dffb8b702a4421c5cbbee
Gaurang Makwana <Gaurang.Makwana@xtremeproductivity.net>
2018-06-01 21:28:05 +0530

    Changed Project Structure
    Changed Project Structure

--- ECommerceDemoApp/ECommerceDemoApp/DesignModules/ProductRankingVC/ProductRankingVC.swift
+++ ECommerceDemoApp/ECommerceDemoApp/DesignModules/ProductRankingVC/ProductRankingVC.swift
@@ -38,3 +38,37 @@ class ProductRankingVC: CustomViewController  {
         self.title = ApplicationTitles.rankingTitle
     }
 }
+
+//Mark: - UITableViewDataSource
+extension ProductRankingVC: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCellIdentifiers.subCategoryCell, for: indexPath)
+        return cell
+    }
+}
+
+//Mark: - UITableViewDelegate
+extension ProductRankingVC: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let categoryCell = tableView.dequeueReusableCell(withIdentifier: TableViewCellIdentifiers.categoryCell)
+        return categoryCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
